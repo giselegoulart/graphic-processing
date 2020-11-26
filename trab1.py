8ada22a6f514831ed143ba266cf6d87bb1febbbb
giselegoulart <gisa_goulart9@hotmail.com>
2020-11-26 16:25:53 -0300

    imagens exercicio 1

--- trab1.py
+++ trab1.py
@@ -48,7 +48,7 @@ def get_reconstructed_image(raw):
 #==============================================================================    
 
 #     
-pastas = [['Escala de Cinza','./benchmarks/gray8bit/'],
+pastas = [#['Escala de Cinza','./benchmarks/gray8bit/'],
           ['Diversas','./benchmarks/misc/']]
 
 
@@ -58,18 +58,30 @@ for database, path in pastas:
     resultados = []
     list_images = load_images(path)
     for j in list_images:
+        for intensidade in [63,31,15,7,1]:
             pixels = get_image(list_images, j)
             img_size1 = pixels.shape[0]
             img_size2 = pixels.shape[1]
             
             print(j,"--", img_size1,"x", img_size2, "\n")
-            print(pixels, "\n")
-            
+            #print(pixels, "\n")
+            novas_intensidades = pixels.copy()
             for linha in range(img_size1):
                 for coluna in range(img_size2):
-                    if(pixels[linha,coluna]>63):
-                        pixels[linha,coluna]=63
-            
+                    #if(pixels[linha,coluna]>63):
+                        novas_intensidades[linha,coluna] = round((intensidade/255)*pixels[linha,coluna])
+                    
+            #print(novas_intensidades, "\n") 
+            n = j.split('/')[-1]
+            n = n.replace('.png','')
+            reconstructed_image = get_reconstructed_image(novas_intensidades)
+            plt.imshow(reconstructed_image, cmap=plt.cm.gray)
+            plt.grid(False)
+            plt.xticks([])
+            plt.yticks([])
+            #plt.show()
+            plt.savefig(str(database)+"_"+str(n)+"_"+str(intensidade)+".png")
+            plt.clf()
             
 
 
